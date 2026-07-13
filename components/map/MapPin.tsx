421567a30b708486296b6f124d9af7018562e59e
charizdiannefalco <charizdianne.falco@wvsu.edu.ph>
2026-07-13 23:01:00 +0800

    feat: changed pin icons

--- components/map/MapPin.tsx
+++ components/map/MapPin.tsx
@@ -1,4 +1,3 @@
-import { pinColor } from '@/lib/utils'
 import type { LocationType } from '@/types'
 
 interface MapPinProps {
@@ -6,18 +5,16 @@ interface MapPinProps {
   size?: number
 }
 
-export default function MapPin({ type, size = 28 }: MapPinProps) {
+export default function MapPin({ type, size = 58 }: MapPinProps) {
+  const src = type === 'CAFE' ? '/cafe.svg' : '/studyHub.svg'
+
   return (
-    <svg
+    <img
+      src={src}
+      alt={type === 'CAFE' ? 'Cafe' : 'Study Hub'}
       width={size}
       height={size}
-      viewBox="0 0 24 24"
-      fill={pinColor(type)}
-      stroke="white"
-      strokeWidth="1"
       style={{ cursor: 'pointer' }}
-    >
-      <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
-    </svg>
+    />
   )
 }
